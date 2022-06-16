e6600fb5bc480d41ed8d8d409f237fa8b4287c83
user <user@C0908.local>
2022-06-16 17:04:12 +0300

    Initial commit

---
+++ app/src/main/java/com/nt/modul_2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.nt.modul_2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.nt.modul_2.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/nt/modul_2/watch1.java
@@ -0,0 +1,14 @@
+package com.nt.modul_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class watch1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch1);
+    }
+}
